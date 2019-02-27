d871f14693253234cfff844f8d4bdd6ad666f60f
jeff15113 <jeff15113@users.noreply.github.com>
2019-02-27 13:10:40 -0500

    MVP complete

--- index.js
+++ index.js
@@ -2,15 +2,38 @@ const express = require("express");
 //const helmet = require('helmet');
 //const cors = require('cors');
 const bcrypt = require("bcrypt");
+const session = require("express-session");
+const KnexSessionStore = require("connect-session-knex")(session);
 
 const db = require("./dbConfig.js");
 const Users = require("./userdb.js");
 
 const server = express();
 
+const sessionConfig = {
+  name: "monkey",
+  secret: "keep it secret, keep it safe!",
+  cookie: {
+    maxAge: 3600000,
+    secure: false // used over https only
+  },
+  httpOnly: true, // cannot access the cookie from js using document.cookie
+  resave: false,
+  saveUninitialized: false, // GDPR laws against setting cookies automatically
+
+  store: new KnexSessionStore({
+    knex: db,
+    tablename: "sessions",
+    sidfieldname: "sid",
+    createtable: true,
+    clearInterval: 3600000
+  })
+};
+
 //server.use(helmet());
 server.use(express.json());
 //server.use(cors());
+server.use(session(sessionConfig));
 
 server.post("/api/register", (req, res) => {
   let user = req.body;
@@ -20,6 +43,7 @@ server.post("/api/register", (req, res) => {
 
   Users.add(user)
     .then(saved => {
+      req.session.user = saved;
       res.status(201).json(saved);
     })
     .catch(error => {
@@ -34,6 +58,7 @@ server.post("/api/login", (req, res) => {
     .first()
     .then(user => {
       if (user && bcrypt.compareSync(password, user.password)) {
+        req.session.user = user;
         res.status(200).json({ message: `Willkommen, ${user.username}!` });
       } else {
         res.status(401).json({ message: "Invalid Credentials" });
@@ -45,23 +70,10 @@ server.post("/api/login", (req, res) => {
 });
 
 function restricted(req, res, next) {
-  const { username, password } = req.headers;
-
-  if (username && password) {
-    Users.findBy({ username })
-      .first()
-      .then(user => {
-        if (user && bcrypt.compareSync(password, user.password)) {
-          next();
-        } else {
-          res.status(401).json({ message: "You shall not pass!" });
-        }
-      })
-      .catch(error => {
-        res.status(500).json({ message: "Ran into an unexpected error" });
-      });
+  if (req.session && req.session.user) {
+    next();
   } else {
-    res.status(400).json({ message: "Thou lefed thyne fields null" });
+    res.status(401).json({ message: "You shall not pass!" });
   }
 }
 
